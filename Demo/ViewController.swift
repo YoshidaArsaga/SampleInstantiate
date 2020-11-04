7d3a5c727283b6588eaaaf455334ff3a6269a5eb
Hiroki Yoshida <h_yoshida@arsaga.jp>
2020-11-04 16:09:29 +0900

    初期設定

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 吉田寛規 on 2020/11/04.
+//
+
+import UIKit
+import SampleInstantiate
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
