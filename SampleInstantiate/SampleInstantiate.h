750920a31abb3f86074ca6828f34e0356f02b97f
Hiroki Yoshida <h_yoshida@arsaga.jp>
2020-11-04 15:58:13 +0900

    Initial Commit

---
+++ SampleInstantiate/SampleInstantiate.h
@@ -0,0 +1,18 @@
+//
+//  SampleInstantiate.h
+//  SampleInstantiate
+//
+//  Created by 吉田寛規 on 2020/11/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleInstantiate.
+FOUNDATION_EXPORT double SampleInstantiateVersionNumber;
+
+//! Project version string for SampleInstantiate.
+FOUNDATION_EXPORT const unsigned char SampleInstantiateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleInstantiate/PublicHeader.h>
+
+
